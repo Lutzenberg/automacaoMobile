263dcf685ea4b446b4c9dcf28f9b327d8910f32b
Lut <lutpax@gmail.com>
2025-10-21 22:30:37 -0300

    Adiciona scrollDown, atualiza .gitignore e inclui recursos de teste

--- src/test/java/ExemploAutomacaoAppium.java
+++ src/test/java/ExemploAutomacaoAppium.java
@@ -2,6 +2,7 @@ import io.appium.java_client.AppiumBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.net.MalformedURLException;
@@ -10,10 +11,10 @@ import java.time.Duration;
 
 public class ExemploAutomacaoAppium {
 
-    @Test
-    public void primerioExemplo() throws MalformedURLException {
+    AndroidDriver driver;
 
-
+    @Before
+    public void before() throws MalformedURLException {
         UiAutomator2Options options = new UiAutomator2Options();
         options.setPlatformName("Android");
         options.setPlatformVersion("13.0");
@@ -24,15 +25,33 @@ public class ExemploAutomacaoAppium {
         options.setAppPackage("io.appium.android.apis");
         options.setAppActivity(".ApiDemos");
 
-        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
+        driver = new AndroidDriver(new URL("http://127.0.0.1:4723/"), options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
-        driver.findElement(AppiumBy.id("com.android.permissioncontroller:id/continue_button")).click();
-        driver.findElement(AppiumBy.id("android:id/button1")).click();
+    }
+
+    @Test
+    public void primerioExemplo() {
+
         driver.findElement(AppiumBy.accessibilityId("App")).click();
         driver.findElement(AppiumBy.accessibilityId("Activity")).click();
         driver.findElement(AppiumBy.accessibilityId("Hello World")).click();
-        Assert.assertEquals("Hello, World!",driver.findElement(AppiumBy.accessibilityId("Hello, World!")).getText());
+
+        // Valida o texto exibido
+        Assert.assertEquals("Hello, World!", driver.findElement(AppiumBy.id("io.appium.android.apis:id/text")).getText());
+
+        // Valida a activity atual
+        String activity = driver.currentActivity();
+        Assert.assertEquals(".app.HelloWorld", activity);
+    }
+
+    @Test
+    public void scrollDown() {
+        driver.findElement(AppiumBy.accessibilityId("Views")).click();
+        // Documnetacao UiScrollable  https://github.com/appium/appium/issues/16690
+        driver.findElement(AppiumBy.androidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView("
+                + "new UiSelector().text(\"WebView\"));"));
+
 
     }
 }
